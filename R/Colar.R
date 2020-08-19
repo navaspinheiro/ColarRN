5aa864c43681ccc0035a096cc7a64730ad5c1058
Roberto Navas <navaspinheiro@gmail.com>
2020-08-19 20:07:52 -0300

    Initial commit

---
+++ R/Colar.R
@@ -0,0 +1,13 @@
+#' Função Colar
+#'
+#' Esta função faz o Paste/Colar de duas variáveis recebidas
+#'
+#' @param a primeiro parâmetro da função
+#' @param b segund parâmetro da função
+#'
+#' @return retorna o resultado do Paste das duas variáveis recebidas
+#'
+#' @export
+colar <- function(a = NULL , b = NULL) {
+  paste(a, b)
+}
